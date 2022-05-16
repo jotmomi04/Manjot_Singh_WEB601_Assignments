b2b81f2e0434e22b0c895d8b5400ae58900bf979
jotmomi04 <91279286+jotmomi04@users.noreply.github.com>
2022-05-15 20:30:24 -0400

    create content

---
+++ MSFavouritePlaces/src/app/models/content.ts
@@ -0,0 +1,9 @@
+export interface Content {
+    readonly id: number;
+    placeName : string;
+    Country: string;
+    Rank: number;
+    discription?:string;
+    imageLink?:string;
+    hashtag?:string[];
+}
