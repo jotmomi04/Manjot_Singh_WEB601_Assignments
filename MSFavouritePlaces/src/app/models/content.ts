d7f9c89596a90abb807909f5324310a365676d09
jotmomi04 <91279286+jotmomi04@users.noreply.github.com>
2022-05-17 22:13:02 -0400

    Update Content and add data

---
+++ MSFavouritePlaces/src/app/models/content.ts
@@ -0,0 +1,12 @@
+export interface Content {
+
+  readonly id: number;
+  placeName : string;
+  Country: string;
+  Rank: number;
+  discription?:string;
+  imageLink?:string;
+  hashtag?:string[];
+
+
+}
