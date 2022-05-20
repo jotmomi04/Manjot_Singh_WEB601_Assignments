fba617b177fbd6fb342116e3c5cf5d6166da907b
jotmomi04 <91279286+jotmomi04@users.noreply.github.com>
2022-05-20 01:59:52 -0400

    make changes with class assigment sample, add addcontent method and create method for readfriendly

--- MSFavouritePlaces/src/app/models/list.ts
+++ MSFavouritePlaces/src/app/models/list.ts
@@ -5,23 +5,37 @@ export class List {
   private _items: Content[];
   constructor() {
     this._items = []; // initialize array
-    this.increaseCount();
+  
 
   }
   get items():Content[] {
     return this._items;}
 
-  increaseCount() {
-    return ++List.contentCount;
+
+ addContent(place:Content):void{
+   this._items.push(place);
   }
- // addContent(id:number,placeName:string,Country:string,Rank:number,discription:string,imageLink:string,hashtag:string[]){
- //   this._items.push( id=id,placeName=placeName,Country=Country,Rank=Rank,imageLink=imageLink,hashtag=hashtag
-//  );
-  //}
 
   amount(): number {
     return this._items.length;
   }
+displayPlace(index: number):string{
+  if (this._items.length <= index || index < 0) {
+    return `<h2>Error, invalid index requested: ${index}</h2>`;
+  }
+  return `
+<table>
+    <td>${this._items[index].id}</td>
+    <td>${this._items[index].placeName}</td>
+    <td>${this._items[index].Country}</td>
+    <td>${this._items[index].Rank}</td>
+    <td>${this._items[index].discription}</td>
+    <td>${this._items[index].hashtag}</td>
+    <td><img src="${this._items[index].imageLink}"/></td>
+</table>
+  
+  `
 
+}
 
 }
