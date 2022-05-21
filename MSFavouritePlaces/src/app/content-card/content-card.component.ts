273406b96d360f650590032996cf5a700a2aaa69
jotmomi04 <91279286+jotmomi04@users.noreply.github.com>
2022-05-20 20:00:03 -0400

    Add ngif for hide optional content

--- MSFavouritePlaces/src/app/content-card/content-card.component.ts
+++ MSFavouritePlaces/src/app/content-card/content-card.component.ts
@@ -12,5 +12,5 @@ export class ContentCardComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+ 
 }
--- MSFavouritePlaces/src/app/content-list/content-list.component.ts
+++ MSFavouritePlaces/src/app/content-list/content-list.component.ts
@@ -13,7 +13,7 @@ places:Content[];
       id: 1,
       placeName: "Nigra Falls",
       Country: "Canada",
-      Rank: 1,
+      Rank: 4,
       discription: "The Official City of Niagara Falls, Ontario Canada website! Interactive Maps, search Cemeteries, Niagara weather and tourism information and more! ",
       imageLink: "https://th.bing.com/th/id/R.77064801323f39fef3d817184e8ff23b?rik=4sSRBA3z3%2fX7iw&pid=ImgRaw&r=0",
       hashtag: ["best Place", "high visitors"]
