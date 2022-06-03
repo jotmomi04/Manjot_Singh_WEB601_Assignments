f786ae1e05efbfdf3471d057835c378d33568c14
jotmomi04 <91279286+jotmomi04@users.noreply.github.com>
2022-06-03 11:33:29 -0400

    create direcetive and assign to html tags

--- MSFavouritePlaces/src/app/app.module.ts
+++ MSFavouritePlaces/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { FilterContentPipe } from './filter-content.pipe';
+import { HighlighterImportantDataDirective } from './highlighter-important-data.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentListComponent,
     ContentCardComponent,
-    FilterContentPipe
+    FilterContentPipe,
+    HighlighterImportantDataDirective
   ],
   imports: [
     BrowserModule
---
+++ MSFavouritePlaces/src/app/highlighter-important-data.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HighlighterImportantDataDirective } from './highlighter-important-data.directive';
+
+describe('HighlighterImportantDataDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HighlighterImportantDataDirective();
+    expect(directive).toBeTruthy();
+  });
+});
