dcafac7a35caf7e7c143f6804e55741b1c72daff
jotmomi04 <91279286+jotmomi04@users.noreply.github.com>
2022-06-10 10:37:51 -0400

    Change version of file and create mock list file in data folder,
    The data no longer in contentList.ts file

---
+++ MSFavouritePlaces/src/app/data/mock-contentList-db.ts
@@ -0,0 +1,56 @@
+import { Content } from "../models/content";
+
+export const PLACES:Content[]=[{
+    id: 1,
+    placeName: "Nigra Falls",
+    Country: "Canada",
+    Type: "Water",
+    discription: "The Official City of Niagara Falls, Ontario Canada website! Interactive Maps, search Cemeteries, Niagara weather and tourism information and more! ",
+    imageLink: "https://th.bing.com/th/id/R.77064801323f39fef3d817184e8ff23b?rik=4sSRBA3z3%2fX7iw&pid=ImgRaw&r=0",
+    hashtag: ["best Place", "high visitors"]
+  },{
+    id: 2,
+    placeName: "Taj Mahal",
+    Country: "India",
+    Type:"Building",
+    discription: "The Taj Mahal, is an ivory-white marble mausoleum on the right bank of the river Yamuna in the Indian city of Agra",
+    imageLink: ""
+    ,  hashtag: ["best Place", "high visitors"]
+  },{ id: 3,
+    placeName: "Harmandir Sahib",
+    Country: "India",
+    
+    discription: "Punjab is a state in northern India. Forming part of the larger Punjab region of the Indian subcontinent",
+    imageLink: "https://www.bing.com/th?id=OIP.kcoOW-3r-CuUHfZE06ZNwwHaEK&w=333&h=187&c=8&rs=1&qlt=90&o=6&dpr=1.5&pid=3.1&rm=2",
+    hashtag: ["best Place", "high visitors"]},
+    { id: 4,
+      placeName: "Venice",
+      Country: "Italy",
+      Type: "Water",
+      discription: "Venice is a city in northeastern Italy and the capital of the Veneto region. It is built on a group of 118 small islands that are separated by canals and linked by over 400 bridges",
+      imageLink:"https://th.bing.com/th/id/OIP.nbWreLvNRy6V2f1upaBnzwHaE7?w=252&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+      hashtag: ["best","water", "high visitors"]},
+      {
+        id: 5,
+        placeName: "eiffel Tower",
+        Country: "Paris",
+        Type: "Tower",
+        discription: "The Eiffel Tower is a wrought-iron lattice tower on the Champ de Mars in Paris, France. It is named after the engineer Gustave Eiffel",
+        imageLink: "https://th.bing.com/th/id/OIP.1wzJdl2EU3BIcih_gWBTagHaJO?w=186&h=233&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+        hashtag: ["best Place", "high visitors"]
+      }, {
+        id: 6,
+        placeName: "Rome",
+        Country: "Italy",
+        discription: "The Eternal City, Rome is worth a visit at least once, with its beautiful architecture and monuments, great works of art, small-town charm, slow pace of life, and world-renowned food, albeit seeing everything it has to offer would take a lifetime.",
+        imageLink: "https://th.bing.com/th/id/OIP.fbOMMSfOIhZB2PR7Kg4cvwHaE8?pid=ImgDet&rs=1",
+        hashtag: ["best Place", "high visitors"]
+      }, {
+        id: 7,
+        placeName: "machu Picchu",
+        Country: "Peru",
+        Type: "Water",
+        discription: "Machu Picchu is a 15th-century Inca citadel located in the Eastern Cordillera of southern Peru on a 2,430-meter (7,970 ft) mountain ridge.",
+        imageLink: "https://th.bing.com/th/id/OIP.QoklOYgkvZO5Umyv-siXrgHaE8?pid=ImgDet&rs=1",
+        hashtag: ["best Place", "high visitors"]
+      }]
