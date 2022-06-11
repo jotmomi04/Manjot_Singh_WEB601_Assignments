dcb2c2c49a6856b4991045d3709ca183353f10d5
jotmomi04 <91279286+jotmomi04@users.noreply.github.com>
2022-06-10 21:56:05 -0400

    update service method

--- MSFavouritePlaces/src/app/services/places.service.ts
+++ MSFavouritePlaces/src/app/services/places.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { PLACES } from '../data/mock-contentList-db';
+import { Content } from '../models/content';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,20 @@ import { Injectable } from '@angular/core';
 export class PlacesService {
 
   constructor() { }
+  getContent(): Observable<Content[]> {
+    return of(PLACES);
+  }
+  getContentItem(id: number):Observable<Content>{
+    return of(PLACES[id]);
+  }
+  addContentItem(content:Content):Observable<Content[]>{
+return  of (PLACES);
+  }  
+  updateContentItem(content:Content):Observable<Content[]>{
+    return  of (PLACES);
+  }
+  deleteContentItem(id:number):Observable<Content>{
+    return of(PLACES[id])
+  }
+
 }
