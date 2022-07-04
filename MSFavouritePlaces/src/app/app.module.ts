6260c1d490a242bed8f7783edbd95a016448b7b8
jotmomi04 <91279286+jotmomi04@users.noreply.github.com>
2022-07-04 00:59:32 -0400

    Change version and Add new blank component

--- MSFavouritePlaces/src/app/app.module.ts
+++ MSFavouritePlaces/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { ContentListComponent } from './content-list/content-list.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { FilterContentPipe } from './filter-content.pipe';
 import { HighlighterImportantDataDirective } from './highlighter-important-data.directive';
+import { DetailContentComponent } from './detail-content/detail-content.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { HighlighterImportantDataDirective } from './highlighter-important-data.
     ContentListComponent,
     ContentCardComponent,
     FilterContentPipe,
-    HighlighterImportantDataDirective
+    HighlighterImportantDataDirective,
+    DetailContentComponent
   ],
   imports: [
     BrowserModule
---
+++ MSFavouritePlaces/src/app/detail-content/detail-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-content',
+  templateUrl: './detail-content.component.html',
+  styleUrls: ['./detail-content.component.scss']
+})
+export class DetailContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
